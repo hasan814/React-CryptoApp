8eb2fd207f07f8cb67a14c428f8baf72f58b3b29
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-20 08:46:26 +0330

    Display Coin in Table

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,12 +1,55 @@
 import { CryptoContext } from "../../context/CryptoContext";
 import { useContext } from "react";
+import { v4 } from "uuid";
+
+import chartDown from "../../assets/chart-down.svg";
+import chartUp from "../../assets/chart-up.svg";
 
 const TableCoin = () => {
   // ============ Context ============
   const { cryptoData } = useContext(CryptoContext);
 
   // ============ Rendering ============
-  return <div>TableCoin</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {cryptoData.map((coin) => (
+            <tr key={v4()}>
+              <td>
+                <div>
+                  <img src={coin.image} alt="" />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </td>
+              <td>{coin.name}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              <td>{coin.total_volume.toLocaleString()}</td>
+              <td>
+                <img
+                  src={
+                    coin.price_change_percentage_24h > 0 ? chartUp : chartDown
+                  }
+                  alt={coin.name}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TableCoin;
